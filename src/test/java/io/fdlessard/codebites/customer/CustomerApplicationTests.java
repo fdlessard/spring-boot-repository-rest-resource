1a90468d5d887f8cebeb7cc7ef94410aa3b11f51
fdlessard <fdlessard@gmail.com>
2020-01-12 18:33:23 -0500

    added elasticbeanstalk

--- src/test/java/io/fdlessard/codebites/customer/CustomerApplicationTests.java
+++ src/test/java/io/fdlessard/codebites/customer/CustomerApplicationTests.java
@@ -3,10 +3,12 @@ package io.fdlessard.codebites.customer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("development-h2")
 public class CustomerApplicationTests {
 
     @Test
