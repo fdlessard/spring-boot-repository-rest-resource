904b3c24fb737718331d2c74d77b64fa49977b46
fdlessard <fdlessard@gmail.com>
2020-01-12 18:02:59 -0500

    Added postgres configuration and profile

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -17,6 +17,8 @@ INSERT INTO customer_category(category) VALUES ('customer-category-2');
 
 
 INSERT INTO customer(category_id, company, first_name, last_name) VALUES (1, 'company-1', 'first-name-1', 'last-name-1');
+INSERT INTO customer(category_id, company, first_name, last_name) VALUES (1, 'company-2', 'first-name-2', 'last-name-2');
+
 
 INSERT INTO address(number, street, city, postal_code, province, country, customer_id) VALUES ('1', 'street-1', 'city-1', 'postalCode-1', 'province-1', 'country-1', 1);
 INSERT INTO address(number, street, city, postal_code, province, country, customer_id) VALUES ('1', 'street-2', 'city-2', 'postalCode-2', 'province-2', 'country-2', 1);
